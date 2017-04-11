9ba49013dd613457e10ad2673fef8918442c6ec0
alexcc05 <alexander-cc@hotmail.com>
2017-04-11 18:03:59 +0100

    v3

--- nn_mnist.py
+++ nn_mnist.py
@@ -53,21 +53,20 @@ y_ = tf.placeholder("float", [None, 10])  # labels
 W1 = tf.Variable(np.float32(np.random.rand(28*28,100)) * 0.1)
 b1 = tf.Variable(np.float32(np.random.rand(100)) * 0.1)
 
-W2 = tf.Variable(np.float32(np.random.rand(100,100)) * 0.1)
-b2 = tf.Variable(np.float32(np.random.rand(100)) * 0.1)
+W2 = tf.Variable(np.float32(np.random.rand(100,10)) * 0.1)
+b2 = tf.Variable(np.float32(np.random.rand(10)) * 0.1)
 
 h = tf.nn.sigmoid(tf.matmul(x, W1) + b1)
 # h = tf.matmul(x, W1) + b1  # Try this!
-y = tf.nn.softmax(tf.matmul(h, W2) + b2) #implementación modelo
+y = tf.nn.softmax(tf.matmul(h, W2) + b2) #implementacion modelo
 
 loss = tf.reduce_sum(tf.square(y_ - y))
 
 train = tf.train.GradientDescentOptimizer(0.01).minimize(loss)  # learning rate: 0.01
 
-init = tf.initialize_all_variables()
 
-sess = tf.Session()
-sess.run(init)
+sess = tf.InteractiveSession()
+sess.run(tf.global_variables_initializer())
 
 print "----------------------"
 print "   Start training...  "
@@ -75,14 +74,20 @@ print "----------------------"
 
 batch_size = 20
 
-for epoch in xrange(10):  #numero neuronas
+for epoch in xrange(85):  #numero epocas
     for jj in xrange(len(train_x) / batch_size):
         batch_xs = train_x[jj * batch_size: jj * batch_size + batch_size]
         batch_ys =train_y[jj * batch_size: jj * batch_size + batch_size]
         sess.run(train, feed_dict={x: batch_xs, y_: batch_ys})
 
     print "Epoch #:", epoch, "Error: ", sess.run(loss, feed_dict={x: batch_xs, y_: batch_ys})
-    #result = sess.run(y, feed_dict={x: batch_xs})
+    result = sess.run(y, feed_dict={x: batch_xs})
     #for b, r in zip(batch_ys, result):
         #print b, "-->", r
     print "----------------------------------------------------------------------------------"
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_, 1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+print(sess.run(accuracy, feed_dict={x: test_x, y_: test_y}))
